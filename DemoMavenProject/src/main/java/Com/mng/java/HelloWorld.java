889cd234d718eb5118b29628c91f68520fd44576
NagendraMekala <nagendra.kldm@gmail.com>
2018-12-01 20:03:57 -0800

    new maven project for jenkins

---
+++ DemoMavenProject/src/main/java/Com/mng/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package Com.mng.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World well comes to jenkens world" );
+    }
+}
